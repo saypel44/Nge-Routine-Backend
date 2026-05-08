705c0e6a630baa3bf973988680994b61f37331e0
Sonam Peldon <03230293.gcbs@rub.edu.bt>
2026-05-08 15:16:32 +0600

    Files

--- spttool.js
+++ spttool.js
@@ -3,7 +3,7 @@
 ═══════════════════════════════════════ */
 
 // const API =""
-
+const API_BASE = 'https://nge-routine-backend.onrender.com';
 let currentUser = null;
 let currentAlarmHabit = null;
 
